02e2b5b43b291a811c2ca78044a6bf3dc800395c
kashyap.shruti11@gmail.com <K@shyapfamily23>
2024-02-06 23:17:44 +0530

    Controller

--- src/main/java/com/shruti/taskmanager/controller/TaskController.java
+++ src/main/java/com/shruti/taskmanager/controller/TaskController.java
@@ -3,10 +3,10 @@ package com.shruti.taskmanager.controller;
 import com.shruti.taskmanager.model.Task;
 import com.shruti.taskmanager.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -14,32 +14,43 @@ import java.util.List;
 public class TaskController {
 
     @Autowired
-    TaskService taskService;
+    private TaskService taskService;
 
     @GetMapping
-    public ResponseEntity<List<Task>> getAllTasks(){
+    public ResponseEntity<List<Task>> getAllTasks() {
         List<Task> tasks = taskService.getAllTasks();
-       return taskService.getAllTasks();
-
+        return new ResponseEntity<>(tasks, HttpStatus.OK);
     }
 
-    @GetMapping("/task/{id}")
-    Task getTaskById(Long id){
-        return  taskService.getTaskById(id);
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+        Task task = taskService.getTaskById(id);
+        if (task != null) {
+            return new ResponseEntity<>(task, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
-    @PostMapping("/savetask")
-    Task saveTask(Task task){
-        return taskService.saveTask(task);
+    @PostMapping
+    public ResponseEntity<Task> saveTask(@RequestBody Task task) {
+        Task savedTask = taskService.saveTask(task);
+        return new ResponseEntity<>(savedTask, HttpStatus.CREATED);
     }
 
-    @PutMapping("/updatetask")
-    Task updateTask(Long id, Task updatedTask){
-        return taskService.updateTask(id,updatedTask);
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
+        Task updated = taskService.updateTask(id, updatedTask);
+        if (updated != null) {
+            return new ResponseEntity<>(updated, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
-    @DeleteMapping("/deleteTask")
-    void deleteTask(Long id){
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
         taskService.deleteTask(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
